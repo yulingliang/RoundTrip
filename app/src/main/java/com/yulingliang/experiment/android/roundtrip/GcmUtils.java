11617dff6e8f06945c74a37480e2a24259dcc698
Yuling Liang <yuling.code@gmail.com>
2014-02-09 17:15:20 -0800

    Ping gcm server and register the device.

---
+++ app/src/main/java/com/yulingliang/experiment/android/roundtrip/GcmUtils.java
@@ -0,0 +1,197 @@
+package com.yulingliang.experiment.android.roundtrip;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import java.io.IOException;
+
+/**
+ * Created by yulingliang on 2/9/14.
+ */
+public final class GcmUtils {
+    private static final String TAG = "GcmUtils";
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private static final String PROPERTY_APP_VERSION = "appVersion";
+    private static final String PROPERTY_REG_ID = "registration_id";
+
+
+    private static GoogleCloudMessaging gcm;
+    private static String regid;
+
+    private GcmUtils() {
+    }
+
+    /**
+     * Check the device to make sure it has the Google Play Services APK. If
+     * it doesn't, display a dialog that allows users to download the APK from
+     * the Google Play Store or enable it in the device's system settings.
+     */
+    public static boolean checkPlayServices(Context context, Activity activity) {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(context);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, activity,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+            }
+            return false;
+        }
+        return true;
+    }
+
+    public static void checkAndMaybeRegisterGcm(Context context, Activity activity, String senderId,
+                                                GcmRegistrationCallback callback) {
+        if (GcmUtils.checkPlayServices(context, activity)) {
+            Log.d(TAG, "Play Service is available.");
+            gcm = GoogleCloudMessaging.getInstance(context);
+            regid = getRegistrationId(context);
+
+            if (regid.isEmpty()) {
+                registerInBackground(context, senderId, callback);
+            } else {
+                callback.onGcmRegistrationFinished("Device already registered.");
+            }
+        } else {
+            Log.d(TAG, "Play Service is NOT available.");
+        }
+    }
+
+    /**
+     * Gets the current registration ID for application on GCM service.
+     * <p/>
+     * If result is empty, the app needs to register.
+     *
+     * @return registration ID, or empty string if there is no existing
+     * registration ID.
+     */
+    private static String getRegistrationId(Context context) {
+        final SharedPreferences prefs = getGCMPreferences(context);
+        String registrationId = prefs.getString(PROPERTY_REG_ID, "");
+        if (registrationId.isEmpty()) {
+            Log.i(TAG, "Registration not found.");
+            return "";
+        }
+        // Check if app was updated; if so, it must clear the registration ID
+        // since the existing regID is not guaranteed to work with the new
+        // app version.
+        int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
+        int currentVersion = getAppVersion(context);
+        if (registeredVersion != currentVersion) {
+            Log.i(TAG, "App version changed.");
+            return "";
+        }
+        return registrationId;
+    }
+
+    /**
+     * @return Application's {@code SharedPreferences}.
+     */
+    private static SharedPreferences getGCMPreferences(Context context) {
+        // This sample app persists the registration ID in shared preferences, but
+        // how you store the regID in your app is up to you.
+        return context.getSharedPreferences(MainActivity.class.getSimpleName(),
+                Context.MODE_PRIVATE);
+    }
+
+    /**
+     * @return Application's version code from the {@code PackageManager}.
+     */
+    private static int getAppVersion(Context context) {
+        try {
+            PackageInfo packageInfo = context.getPackageManager()
+                    .getPackageInfo(context.getPackageName(), 0);
+            return packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            // should never happen
+            throw new RuntimeException("Could not get package name: " + e);
+        }
+    }
+
+    /**
+     * Registers the application with GCM servers asynchronously.
+     * <p/>
+     * Stores the registration ID and app versionCode in the application's
+     * shared preferences.
+     */
+    private static void registerInBackground(final Context context, final String senderId,
+                                             final GcmRegistrationCallback callback) {
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... params) {
+                String msg = "";
+                try {
+                    if (gcm == null) {
+                        gcm = GoogleCloudMessaging.getInstance(context);
+                    }
+                    regid = gcm.register(senderId);
+                    msg = "Device registered, registration ID=" + regid;
+
+                    // You should send the registration ID to your server over HTTP,
+                    // so it can use GCM/HTTP or CCS to send messages to your app.
+                    // The request to your server should be authenticated if your app
+                    // is using accounts.
+                    sendRegistrationIdToBackend();
+
+                    // For this demo: we don't need to send it because the device
+                    // will send upstream messages to a server that echo back the
+                    // message using the 'from' address in the message.
+
+                    // Persist the regID - no need to register again.
+                    storeRegistrationId(context, regid);
+                } catch (IOException ex) {
+                    msg = "Error :" + ex.getMessage();
+                    // If there is an error, don't just keep trying to register.
+                    // Require the user to click a button again, or perform
+                    // exponential back-off.
+                }
+                return msg;
+            }
+
+            @Override
+            protected void onPostExecute(String msg) {
+                callback.onGcmRegistrationFinished(msg);
+            }
+        }.execute(null, null, null);
+    }
+
+    /**
+     * Sends the registration ID to your server over HTTP, so it can use GCM/HTTP
+     * or CCS to send messages to your app. Not needed for this demo since the
+     * device sends upstream messages to a server that echoes back the message
+     * using the 'from' address in the message.
+     */
+    private static void sendRegistrationIdToBackend() {
+        // Your implementation here.
+    }
+
+    /**
+     * Stores the registration ID and app versionCode in the application's
+     * {@code SharedPreferences}.
+     *
+     * @param context application's context.
+     * @param regId   registration ID
+     */
+    private static void storeRegistrationId(Context context, String regId) {
+        final SharedPreferences prefs = getGCMPreferences(context);
+        int appVersion = getAppVersion(context);
+        Log.i(TAG, "Saving regId on app version " + appVersion);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(PROPERTY_REG_ID, regId);
+        editor.putInt(PROPERTY_APP_VERSION, appVersion);
+        editor.commit();
+    }
+
+    public interface GcmRegistrationCallback {
+        public void onGcmRegistrationFinished(String msg);
+    }
+}
--- app/src/main/java/com/yulingliang/experiment/android/roundtrip/MainActivity.java
+++ app/src/main/java/com/yulingliang/experiment/android/roundtrip/MainActivity.java
@@ -1,39 +1,53 @@
 package com.yulingliang.experiment.android.roundtrip;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.text.format.Time;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 // This is the generated main activity class.
 public class MainActivity extends ActionBarActivity {
-    private final static String TAG = "MainActivity";
-    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    final String TAG = "RoundTrip.MainActivity";
+    public static final String EXTRA_MESSAGE = "message";
+
+    private static final String FRAGMENT_TAG = "MAIN_FRAGMENT";
+
+    AtomicInteger msgId = new AtomicInteger();
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (checkPlayServices()) {
-            Log.d(TAG, "Play Service is available.");
-        } else {
-            Log.d(TAG, "Play Service is NOT available.");
-        }
+        context = getApplicationContext();
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new PlaceholderFragment(), FRAGMENT_TAG)
                     .commit();
         }
     }
@@ -58,40 +72,55 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * Check the device to make sure it has the Google Play Services APK. If
-     * it doesn't, display a dialog that allows users to download the APK from
-     * the Google Play Store or enable it in the device's system settings.
-     */
-    private boolean checkPlayServices() {
-        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
-        if (resultCode != ConnectionResult.SUCCESS) {
-            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
-                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
-                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
-            } else {
-                Log.i(TAG, "This device is not supported.");
-                finish();
-            }
-            return false;
-        }
-        return true;
-    }
 
+
+    private void appendLog(String log) {
+        PlaceholderFragment fragment =
+                (PlaceholderFragment) getSupportFragmentManager().findFragmentByTag(FRAGMENT_TAG);
+        fragment.appendLog(log);
+    }
 
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class PlaceholderFragment extends Fragment implements GcmUtils.GcmRegistrationCallback  {
+        private static final String TAG = "RoundTrip.PlaceholderFragment";
+        /**
+         * Substitute you own sender ID here. This is the project number you got
+         * from the API Console, as described in "Getting Started."
+         */
+        private static final String SENDER_ID = "503276688513";
 
+        private List<String> mLogMessages = new ArrayList<String>();
+        private ArrayAdapter<String> mAdapter;
+        private Time mTime = new Time(Time.getCurrentTimezone());
         public PlaceholderFragment() {
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
+            Log.d(TAG, "onCreateView");
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            mAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, mLogMessages);
+            ListView logList = (ListView) rootView.findViewById(R.id.log_messages);
+            logList.setAdapter(mAdapter);
+
+
+            GcmUtils.checkAndMaybeRegisterGcm(getActivity(), getActivity(), SENDER_ID, this);
+
             return rootView;
+        }
+
+        public void appendLog(String logMessage) {
+            mTime.setToNow();
+            mLogMessages.add(0, mTime.format("%k:%M:%S") + ": " + logMessage);
+            mAdapter.notifyDataSetChanged();
+        }
+
+        @Override
+        public void onGcmRegistrationFinished(String msg) {
+            appendLog(msg);
         }
     }
 
